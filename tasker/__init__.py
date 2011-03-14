8989237c768a01fb67983b7b33482670594e0230
Vincent Petithory <vincent.petithory@gmail.com>
2011-03-14 22:25:32 +0100

    Wrapped a task list object into a Tasker object
     * encapuslates all the logic of running a tasks module
       into a Tasker class

--- tasker/__init__.py
+++ tasker/__init__.py
@@ -4,12 +4,129 @@ version_info = (0,1,0)
 
 import sys
 import os
+import imp
 
-TASK_LIST = []
-DEFAULT_TASKNAME = None
-TASK_MODULE = None
-TASK_MODULENAME = None
-TASKS = None
+import tasker.utils
+
+class Tasker(object):
+    
+    taskers = []
+    
+    @staticmethod
+    def get_tasker_for_modulename(module_name):
+        for tasker_obj in Tasker.taskers:
+            if tasker_obj.task_modulename == module_name:
+                return tasker_obj
+        return None
+    
+    task_list = []
+    taskfunc_list = []
+    default_taskname = None
+    task_module = None
+    task_modulename = None
+    tasks = None
+    
+    EXTRA_IMPORTS = {
+        'os': os,
+        'sys': sys,
+        'sh': tasker.utils.sh,
+        'shp': tasker.utils.shp,
+        'shell': tasker.utils.shell,
+        'shellp': tasker.utils.shellp,
+        'tar': tasker.utils.tar,
+        'call_task': tasker.utils.call_task,
+    }
+    
+    def __init__(self, task_file):
+        Tasker.taskers.append(self)
+        self.module_path = os.path.realpath(task_file)
+        self.module_dirname = os.path.dirname(self.module_path)
+        self.module_basename = os.path.basename(self.module_path)
+        
+        self.task_modulename, ext = os.path.splitext(self.module_path)
+        del ext
+        self.task_modulename = self.task_modulename.replace('/', '.')
+        if self.task_modulename.startswith('.'):
+            self.task_modulename = self.task_modulename[1:]
+        
+        # cd in the task module directory by default
+        os.chdir(self.module_dirname)
+        
+        # import the tasks module
+        try:
+            self.module_file = open(self.module_path, 'r')
+            self.task_module = imp.load_module(self.task_modulename, self.module_file, self.module_path, ('.py', 'r', imp.PY_SOURCE))
+            self.module_file.close()
+            self.task_module.__dict__.update(Tasker.EXTRA_IMPORTS)
+        except ImportError as ie:
+            sys.stderr.write('Could not import the « %s » file in the directory « %s ».Reason: %s\n' % (self.task_modulename, self.module_dirname, ie))
+            sys.exit(2)
+        except Exception:
+            sys.stderr.write('Exception caught in your « %s » init code.\n' % self.task_modulename)
+            raise
+        
+    def run(self, task_list):
+        self.task_list = task_list
+        # exit if not tasks defined
+        if len(self.taskfunc_list) == 0: # filled with task() decorator
+            sys.stdout.write('No tasks in « %s ». Exit.\n' % self.task_modulename)
+            sys.exit(0)
+        
+        # get the default target
+        if len(self.task_list) == 0:
+            try:
+                self.default_taskname = getattr(self.task_module, 'DEFAULT')
+            except AttributeError:
+                t1 = self.task_list[0]
+                self.default_taskname = t1[len(t1)-1]
+            else:
+                try:
+                    # check default task name exists, otherwise, exit
+                    getattr(self.task_module, self.default_taskname)
+                except AttributeError:
+                    sys.stderr.write('Could not find the specified default task « %s ».\n' % self.default_taskname)
+                    sys.exit(3)
+                
+        if len(self.task_list) == 0:
+           self.task_list = [self.default_taskname]
+        
+        # execute the init function if defined
+        try:
+            INIT_FUNC = getattr(self.task_module, 'init')
+        except AttributeError:
+            # no init function was defined. Skip.
+            pass
+        else:
+            INIT_FUNC()
+
+        task_objects = []
+
+        def get_task_with_name(name):
+            for task_object in task_objects:
+                if task_object.name == name:
+                    return task_object
+            return None
+
+        for raw_task in self.taskfunc_list:
+            n = len(raw_task)
+            main_taskname = raw_task[n-1]
+            dep_tasknames = []
+            if n > 1:
+                dep_tasknames = raw_task[:n-1]
+            t = Task(name=main_taskname, tasker=self, namedepends=dep_tasknames)
+            task_objects.append(t)
+
+        for task_object in task_objects:
+            task_object.resolve(task_objects, self.task_module)
+
+        for task_name in self.task_list:
+            task = get_task_with_name(task_name)
+            try:
+                task.process()
+            except TaskError as te:
+                sys.stderr.write('** tasker: Exception « %s » caught in task « %s »: %s\n' % (te.exception_type, te.name, te.message))
+                sys.stderr.write('** tasker: Exiting.\n')
+                sys.exit(1)
 
 class TaskError(Exception):
     """An error while running a task."""
@@ -23,9 +140,12 @@ class Task(object):
     """
         depends: list of other Task elements
     """
+    name = None
+    tasker = None
     
-    def __init__(self, name, namedepends=None):
+    def __init__(self, name, tasker, namedepends=None):
         self.name = name
+        self.tasker = tasker
         self.func = None
         if namedepends is None:
             self.namedepends = ()
@@ -37,13 +157,11 @@ class Task(object):
             for dep in self.depends:
                 dep.process()
         try:
-            #print 'exec', type(self.func)
             self.func()
         except Exception as e:
             st = str(type(e))
             exception_type = st[st.find('\'')+1:st.rfind('\'')]
             raise TaskError(str(e), self.name, exception_type)
-            sys.exit(os.EX_SOFTWARE)
     
     def resolve(self, other_tasks, module):
         self.depends = []
@@ -56,14 +174,13 @@ class Task(object):
         # notify which deps could not be resolved
         unresolved_deps = set(self.namedepends).difference(set([t.name for t in self.depends]))
         if len(unresolved_deps) > 0:
-            sys.stderr.write('Could not resolve task(s) « %s ». Exit.\n' % ', '.join(unresolved_deps))
-            sys.exit(os.EX_SOFTWARE)
+            raise AttributeError('Could not resolve task(s) « %s »' % ', '.join(unresolved_deps), self.name, 'TaskError')
         try:
             self.func = getattr(module, self.name)
         except AttributeError:
-            sys.stderr.write('Could not find function « %s » in « %s ».\n' % (self.name, DEFAULT_TASKNAME))
+            sys.stderr.write('Could not find function « %s » in « %s ».\n' % (self.name, self.tasker.task_modulename))
             raise
 
-# import this, as a shortcut when writing the tasks.py files
+# import the task decorator, as a shortcut when writing the tasks.py files
 from tasker.decorators import task
 
--- tasker/decorators.py
+++ tasker/decorators.py
@@ -1,18 +1,21 @@
 #-*- coding:utf-8 -*-
 
-from tasker import TASK_LIST
+from tasker import Tasker
 from functools import wraps
 import collections
 
 class task(object):
-
+    
+    related_tasker = None
+    
     def __init__(self, *args, **kwargs):
         if len(args) > 0:
             f = args[0]
             if isinstance(f, collections.Callable):
                 # the decorator is called without args,
                 # hence 1st arg is our function to decorate
-                TASK_LIST.append((f.__name__, ))
+                self.related_tasker = Tasker.get_tasker_for_modulename(f.__module__)
+                self.related_tasker.taskfunc_list.append((f.__name__, ))
                 self._decorate(f)
             else:
                 self.depends = args
@@ -20,8 +23,9 @@ class task(object):
             self.depends = ()
 
     def __call__(self, f):
-        t = tuple(reversed(self.depends)) + (f.__name__, )
-        TASK_LIST.append(t)
+        t = tuple(self.depends) + (f.__name__, )
+        self.related_tasker = Tasker.get_tasker_for_modulename(f.__module__)
+        self.related_tasker.taskfunc_list.append(t)
         return self._decorate(f)
     
     def _decorate(self, f):
